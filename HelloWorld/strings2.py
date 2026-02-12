ccba0ea2eaae8127220589bcdba367c709a5f2f5
Andre F <50321935+DSFdesigns@users.noreply.github.com>
2026-02-12 22:55:44 +0900

    20260212
    strings2.py updates

---
+++ HelloWorld/strings2.py
@@ -0,0 +1,35 @@
+# 012345678901234
+# parrot = "Norwegian Blue"
+# print(parrot)
+# print(parrot[3])
+
+# Indexing Challenge:
+parrot = "Norwegian Blue"
+# print(parrot)
+
+# print(parrot[3])
+# print(parrot[4])
+# print(parrot[9])
+# print(parrot[3])
+# print(parrot[6])
+# print(parrot[8])
+
+# Negative indexing challenge:
+print()
+# print(parrot[-1])
+# print(parrot[-14])
+# Negative indexing challenge (as above)
+print(parrot[-11])
+print(parrot[-10])
+print(parrot[-5])
+print(parrot[-11])
+print(parrot[-8])
+print(parrot[-6])
+
+print()
+print(parrot[3 - 14])
+print(parrot[4 - 14])
+print(parrot[9 - 14])
+print(parrot[3 - 14])
+print(parrot[6 - 14])
+print(parrot[8 - 14])
